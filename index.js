74797778301ccecc706807e7db444405e7e71aa8
Rafael <trelik.monteiro98@gmail.com>
2020-08-21 09:55:18 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const Discord = require("discord.js");
+
+const bot = new Discord.Client();
+
+const token = "NzQ2MTcyNTcxNzI4MjE2MTE1.Xz8dXA.3eQnoOwWXo4AFhnRxdfQFAI2dIY";
+
+bot.on("ready", () => {
+  console.log("bot running!");
+});
+
+bot.on("message", (msg) => {
+  let message = msg.content.toLocaleLowerCase();
+
+  //Travar o Juan.
+  let result = message.search("https://twitch.tv/trindadelol");
+  if (result != -1) {
+    msg.delete();
+    msg.reply("Hoje não amigo!");
+  }
+
+  //Rola um D20
+  if (message === "=>d20") {
+    let resultado = Math.floor(Math.random() * 21);
+    msg.delete();
+    msg.reply("rodou um D20 e tirou " + resultado + ".");
+  }
+
+  //Apresentar o watch2gether
+  if (message === "=>w2g") {
+    msg.delete();
+    msg.reply(
+      " claro, aqui está o link do nosso watch2gether: https://tinyurl.com/y23p2lyq"
+    );
+  }
+
+  //Aprensentar o Drive
+  if (message === "=>drive") {
+    msg.delete();
+    msg.reply(
+      " claro, aqui está o link do nosso drive: https://tinyurl.com/yyf23k7x"
+    );
+  }
+
+  //Reagir com BIRB
+  if (
+    msg.author.username === "Rainbow Kira" ||
+    msg.author.username === "Netuno"
+  ) {
+    msg.react("439290612324499456");
+  }
+});
+
+bot.login(token);
